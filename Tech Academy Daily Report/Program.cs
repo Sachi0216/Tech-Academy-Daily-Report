07e11ed9eb60f64679b5878fedbfddb99f5c2145
Sachi0216 <davis.elijah15@hotmail.com>
2022-03-27 20:22:38 -0500

    Add project files.

---
+++ Tech Academy Daily Report/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tech_Academy_Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report.");
+
+
+        }
+    }
+}
